184bf6c75ff9740d1aa4cc241bcc41a8e831df6c
mch <mich>
2025-12-03 16:35:07 +0700

    menambahkan tentang.php

---
+++ tentang.php
@@ -0,0 +1,16 @@
+<?php
+$title = "Tentang Prodi";
+include 'partials/header.php';
+?>
+
+<div class="bg-white p-8 rounded-lg shadow-lg">
+    <h1 class="text-4xl font-bold text-gray-800 mb-6">Tentang Program Studi Sistem Informasi Unika Soegijapranata Semarang</h1>
+    
+    <div class="text-gray-700 space-y-4">
+       
+    </div>
+</div>
+
+<?php
+include 'partials/footer.php';
+?>
